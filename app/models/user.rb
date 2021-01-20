625d06033b9a2ad189ec36edb25908541a41b0ee
kohaku428 <daihuku414428@icloud.com>
2021-01-20 21:27:56 +0900

    バリデーションの設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email, uniqueness: true
-  validates :nickname, presence: true
-  validates :first_name, presense: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :email,              uniqueness: true
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true
+  validates :last_name,          presence: true
+  validates :first_name_kana,    presence: true
+  validates :last_name_kana,     presence: true
+  validates :birthday,           presence: true
 
 end

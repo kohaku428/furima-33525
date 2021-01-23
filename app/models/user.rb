d38343ed38b6c956a09d6a01f50056f9b50e6472
kohaku428 <daihuku414428@icloud.com>
2021-01-23 20:48:57 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :email,              uniqueness: { case_sensitive: false }
   validates :nickname,           presence: true
-  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-  validates :first_name_kana,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-  validates :last_name_kana,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   validates :birthday,           presence: true
-  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "Include both letters and numbers"}
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'Include both letters and numbers' }
 end

cba5d3a77399a6d1b1f0d374bcd9621d2daaa7c6
kohaku428 <daihuku414428@icloud.com>
2021-01-29 13:48:56 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,7 @@ class Item < ApplicationRecord
   belongs_to :day
   belongs_to :user
   validates :description, :item_name, :image, presence: true
-  validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }
   validates :category_id, :condition_id, :cost_id, :region_id,
             :day_id, numericality: { other_than: 1 }
 end

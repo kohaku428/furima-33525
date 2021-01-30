4a64f32b23eba737f024d5f87cc3536612383308
kohaku428 <daihuku414428@icloud.com>
2021-01-30 21:39:33 +0900

    同様の記述があるため、メソッドを新しく作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_tweet, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,18 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
@@ -42,5 +40,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:category_id, :image, :condition_id, :cost_id, :day_id, :item_name, :description, :price,
                                  :region_id).merge(user_id: current_user.id)
+  end
+  def set_tweet
+    @item = Item.find(params[:id])
   end
 end

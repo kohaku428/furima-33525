7b3a962ce2512fd8b9784789f243ff32bb4172ff
kohaku428 <daihuku414428@icloud.com>
2021-01-31 12:52:06 +0900

    if文を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,11 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-      @item.destroy
+     if @item.destroy
       redirect_to root_path
+     else
+      render :show
+     end
     else
       render root_path
     end

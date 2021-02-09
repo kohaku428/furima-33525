7a45a6cb2ec0045a0aebdd9b21a2fb47b8f59c0d
kohaku428 <daihuku414428@icloud.com>
2021-02-09 14:11:48 +0900

    決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,21 +5,29 @@ class OrdersController < ApplicationController
   end
    
   def create
-    # binding.pry
     @item = Item.find(params[:item_id])
     @user_orders = UserOrders.new(order_params)
    if @user_orders.valid?
-     @user_orders.save
-     redirect_to root_path
+      pay_item
+      @user_orders.save
+      redirect_to root_path
    else
-     render action: :index
+      render action: :index
    end
   end
 
  private
 
  def order_params
-  params.require(:user_orders).permit(:postal_code, :region_id, :city, :house_number, :building_name, :telephone,).merge(user_id: current_user.id, item_id: params[:item_id],)
+  params.require(:user_orders).permit(:postal_code, :region_id, :city, :house_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
  end
 
+ def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+   amount: @item.price,
+   card: order_params[:token],
+   currency: 'jpy'
+    )
+ end
 end

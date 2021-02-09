5b08e840c4d7fcfc294359d42068530214e0596d
kohaku428 <daihuku414428@icloud.com>
2021-02-09 19:45:58 +0900

    バリデーションの追加

--- app/models/user_orders.rb
+++ app/models/user_orders.rb
@@ -3,13 +3,16 @@ class UserOrders
   attr_accessor :user_id, :item_id, :postal_code, :region_id, :city, :house_number, :building_name, :telephone, :token
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :token
     validates :city
     validates :house_number
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :telephone, format: { with: /\A\d{10}$|^\d{11}\z/ }
+    validates :region_id, numericality: { other_than: 1 }
   end
-  validates :region_id, numericality: { other_than: 1 }
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, region_id: region_id, city: city, house_number: house_number,

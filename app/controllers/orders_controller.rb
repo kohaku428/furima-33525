89ec121e919d594b8e27f69a8c8ed879ae7c5ddf
kohaku428 <daihuku414428@icloud.com>
2021-02-09 14:51:46 +0900

    ログインユーザーによってページの遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
     @user_orders = UserOrders.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
    
   def create

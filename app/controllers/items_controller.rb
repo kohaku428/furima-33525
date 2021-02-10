e337a8f68799dcc9db91a394f25edeb48828f580
kohaku428 <daihuku414428@icloud.com>
2021-02-10 11:49:59 +0900

    売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :sold_out, only: [:edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -22,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
+  redirect_to root_path unless current_user.id == @item.user_id   
   end
 
   def update
@@ -54,5 +55,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def sold_out
+    redirect_to root_path if @item.order.present?
   end
 end
